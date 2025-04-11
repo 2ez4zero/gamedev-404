20a3c9e01a498ed93d946a3a807047f16b6e65ec
2ez4zero <paulinokenn@gmail.com>
2025-04-11 13:51:37 +0800

    asd

---
+++ Assets/scripts/goToRoom3.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToRoom3 : MonoBehaviour
+{
+   public void room3()
+    {
+        SceneManager.LoadScene("room 3");
+    }
+}
